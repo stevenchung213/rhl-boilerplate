4b575b89506f212c6bd68e26469dfbb468917288
Steven Chung <stevenchung.213@gmail.com>
2019-06-29 00:14:10 -0700

    added jest/enzyme. added express compression to serve gzip assets

---
+++ src/components/Main.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+import Main from './Main';
+
+describe('Main', () => {
+
+  it('should render correctly', () => {
+    const component = shallow(<Main/>);
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should have one child node', () => {
+    const component = shallow(<Main/>);
+    expect(component.children()).toHaveLength(1);
+  });
+
+});
