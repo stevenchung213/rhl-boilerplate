f65a08ffae9df40cacebe81cd0909e31125dc73c
Steven Chung <stevenchung.213@gmail.com>
2019-07-15 17:18:35 -0700

    production specified in webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -73,7 +73,7 @@ module.exports = merge(common, {
     }
   },
   plugins: [
-    new webpack.DefinePlugin({ "process.env.NODE_ENV": "production" }),
+    new webpack.DefinePlugin({ "process.env.NODE_ENV": JSON.stringify("production") }),
     new webpack.optimize.AggressiveMergingPlugin(),
     new MiniCssExtractPlugin({
       filename: "[name].css",
