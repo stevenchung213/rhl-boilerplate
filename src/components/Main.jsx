be59110bc025baf5ac89081ceeff71cc4000d616
Steven Chung <stevenchung.213@gmail.com>
2019-12-23 23:23:08 -0800

    jsx import without extension support added

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { hot } from 'react-hot-loader/root';
+
+const Main = () => {
+  return (
+    <div>
+      Hello world!
+    </div>
+  );
+};
+
+export default hot(Main);
--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ render(Main);
 if (module.hot) {
   // keep in mind - here you are configuring HMR to accept CHILDREN MODULE
   // while `hot` would configure HMR for the CURRENT module
-  module.hot.accept('./components/Main.js', () => {
+  module.hot.accept('./components/Main', () => {
     // if you are using harmony modules ({modules:false})
     const NextMain = require('./components/Main').default;
     // in all other cases - re-require App manually
